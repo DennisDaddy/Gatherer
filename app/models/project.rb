648ee4798c713b07861c955f49ee6623fa7583db
DennisDaddy <softteez95@gmail.com>
2019-02-21 14:34:45 +0300

    Write simple first test

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+    def done?
+        true
+    end
+    
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Project do
+  it "considers a project with no tasks to be done" do
+    project = Project.new
+    expect(project.done?).to  be_truthy
+  end
 end
