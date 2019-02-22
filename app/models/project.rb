b7129ab98304b62766d9b8365305956016e5dc72
DennisDaddy <softteez95@gmail.com>
2019-02-22 08:46:25 +0300

    Test remaining projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,19 @@ class Project < ApplicationRecord
         @tasks = []
     end
 
+    def incomplete_tasks
+        tasks.reject(&:complete?)
+    end
+
     def done?
         tasks.all?(&:complete?)
-    end   
+    end  
+    
+    def total_size
+        tasks.sum(&:size)
+    end
+
+    def remaining_size
+        tasks.reject(&:complete?).sum(&:size)
+    end
 end
